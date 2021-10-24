dfadfc86f8aea23260918f8548bd793dfa57ebe2
naomysilva <naomyleticia92@gmail.com>
2021-10-24 14:37:50 -0300

    condicao while adicionada

--- jogoDeAdivinhacao.py
+++ jogoDeAdivinhacao.py
@@ -1,23 +1,26 @@
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
-
+total_de_tentativas = 3
+rodada = 1
 numero_secreto = 42
+while (rodada <= total_de_tentativas):
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou " , chute_str)
-chute = int(chute_str)
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+  
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Parabéns! Você acertou!")
-else:
-    if(maior):
-        print("O seu chute foi maior do que o número secreto!")
-    elif(menor):
-        print("O seu chute foi menor do que o número secreto!")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
