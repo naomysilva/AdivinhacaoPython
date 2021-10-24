03cff61224ec47ca687b5e84b087287ed338319c
naomysilva <naomyleticia92@gmail.com>
2021-10-24 17:22:06 -0300

    adicionado funcao random (numero secreto aleatorio)

--- jogoDeAdivinhacao.py
+++ jogoDeAdivinhacao.py
@@ -1,10 +1,13 @@
+import random
+
 total_de_tentativas = 3
 rodada = 1
-numero_secreto = 42
-while (rodada <= total_de_tentativas):
+numero_secreto = random.randrange(1,101)
 
-    print("Tentativa", rodada, "de", total_de_tentativas)
-    chute_str = input("Digite o seu número: ")
+for rodada in range(0, total_de_tentativas):
+
+    print("Tentativa {} de {}".format(rodada + 1, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
   
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
@@ -13,8 +16,14 @@ while (rodada <= total_de_tentativas):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue 
+
+
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
